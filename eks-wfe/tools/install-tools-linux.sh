ccd43a2d76267e138a5509267bd03edb7511b1d8
sureshthumma <suresh@stackaero.io>
2019-07-23 15:22:31 -0500

    update tools install script

--- eks-wfe/tools/install-tools-linux.sh
+++ eks-wfe/tools/install-tools-linux.sh
@@ -1,6 +1,16 @@
 
 curl -o ./tools/heptio-authenticator-aws https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/heptio-authenticator-aws
-chmod +x ./tools/heptio-authenticator-aws
+chmod +x ./tools/heptio-authenticator-aws \
+ && mv ./tools/heptio-authenticator-aws /usr/local/bin/heptio-authenticator-aws
 
 curl -o ./tools/kubectl https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/bin/linux/amd64/kubectl
-chmod +x ./tools/kubectl
+chmod +x ./tools/kubectl \
+ && mv ./tools/kubectl /usr/local/bin/kubectl
+
+curl -o ./tools/helm-v2.11.0-linux-amd64.tar.gz https://storage.googleapis.com/kubernetes-helm/helm-v2.11.0-linux-amd64.tar.gz  \
+    && tar -zxvf ./tools/helm-v2.11.0-linux-amd64.tar.gz  -C /tmp \
+    && mv /tmp/linux-amd64/helm /usr/local/bin/helm \
+    && mv /tmp/linux-amd64/tiller /usr/local/bin/tiller \
+    && chmod +x /usr/local/bin/tiller \
+    && chmod +x /usr/local/bin/helm \
+    && rm -f ./tools/helm-v2.11.0-linux-amd64.tar.gz
